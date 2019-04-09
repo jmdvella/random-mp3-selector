2e721fb2e8ec40c7027ec487fd8e714f47a8659c
jmdvella <41462638+jmdvella@users.noreply.github.com>
2019-04-09 06:43:35 +0200

    Update random-mp3-selector.py

--- random-mp3-selector.py
+++ random-mp3-selector.py
@@ -2,24 +2,56 @@ import os
 import shutil
 import random
 
+
 # source directory for files to copy from
-source_dir = "E:\\_Temp\\Music"
+while True:
+    source_dir = input('Type the directory of your music (or copy and paste):\n')
+    # check the directory exists 
+    if os.path.isdir(source_dir):
+        break
+    else:
+        print('\n#####\nThis diretory does not exist, please check you have typed the path correctly\n')
+
+# create output directory, if exists add to this directory
+os.makedirs(os.path.expanduser('~\Desktop\\CollectedMusic'), exist_ok=True)
+
 # target directory for files to copy to
-target_dir = "C:\\test\\to"  
+target_dir = os.path.expanduser('~\Desktop\\CollectedMusic')
 
  # empty list for collecting files
 source_files = []  
 
-# walk through directory tree and find files only
-for dirpath, dirnames, filenames in os.walk(source_dir):
+# exclude directories
+exclude_dir = input('\nEnter a directory that you would like to exclude.\n(press Enter if you wish to include all directories):\n')
+# give user option to select no directories to exclude
+if exclude_dir == '':
+    exclude_dir = None
+
+# walk through directory tree 
+# select files that are not in excluded directory
+for dirpath, dirnames, filenames in os.walk(source_dir, topdown=True):
+    try:
+        dirnames[:] = [d for d in dirnames if not d.startswith(exclude_dir)]
+    except TypeError:
+        continue
+    # find file names that end with .mp3
     for file in filenames:
         if file.endswith(".mp3"):
             source_files.append(os.path.join(dirpath, file))
 
-# select 300 files randomly               
-choices = random.sample(source_files, 300)
-print(choices)
+# ask user to select how many files to copy  
+while True:
+    try:
+        choices = random.sample(source_files, int(input('How many songs would you like to collect? (must be a number not a word):\n')))
+        break
+    # if more files requested than exist give user another chance to select the amount of files to select
+    except ValueError:
+        print('\n#####\nYou have selected more songs than you have in this directory, try a lower number\n')
 
 # copy files to target directory
 for files in choices:
     shutil.copy(files, target_dir)
+
+# print each song name
+for songs in choices:
+    print(songs)
